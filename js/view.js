b53b53fbef13a21163c30a4af925324dfae5a7cc
kumavis <aaron@kumavis.me>
2017-07-04 02:58:55 -0700

    rearrange view

--- js/view.js
+++ js/view.js
@@ -5,119 +5,132 @@ module.exports = render
 function render(state, actions) {
   return (
 
-    h('div.header', [
-      h('a', {
-        'attributes': {
-          'href': 'https://ipfs.io',
-          'target': '_blank'
-        }
-      }, [
-        h('img#logo', {
+    h('div', [
+
+      h('div.header', [
+        h('a', {
           'attributes': {
-            'src': 'ipfs-logo.svg',
-            'height': '32'
-          },
-          'id': {
-            'name': 'id',
-            'value': 'logo'
+            'href': 'https://ipfs.io',
+            'target': '_blank'
           }
-        })
-      ]),
-    ]),
-    h('div.wrapper', [
-      h('div#ipfs.ipfs', [
-        h('div.left.panel', [
-          h('div#details'+ state.peerInfo.addresses ? '' : '.disabled', [
-            h('h2', `Your daemon`),
-            h('h3', `ID`),
-            h('pre.id-container', state.peerInfo.id),
-            h('h3', `Addresses`),
-            h('ul.addresses-container', [(
-              state.peerInfo.addresses ?
-                state.peerInfo.addresses.map((address) => h('li', [h('span.address', address)]))
-              : h('li', `Not yet online`)
-            )])
-          ])
+        }, [
+          h('img#logo', {
+            'attributes': {
+              'src': 'ipfs-logo.svg',
+              'height': '32'
+            },
+            'id': {
+              'name': 'id',
+              'value': 'logo'
+            }
+          })
         ]),
-        h('div.right.panel', [
-          h('div#peers'+ state.peers.length ? '' : '.disabled', [
-            h('h2', 'Remote Peers'),
-            (
-              state.peers.length ?
-                h('ul', [
-                  state.peers.map((peer) => h('li', peer.addr.toString()))
-                ])
-              : h('i', 'Waiting for peers...')
-            ),
-          ]),
-          h('div', [
-            h('input.connect-peer', {
+      ]),
+
+      h('div.wrapper', [
+        h('div#ipfs.ipfs', [
+
+          // data lookups
+          h('#resolve-container.panel', [
+            h('h2', `Resolve from network`),
+            h('input', {
               'attributes': {
                 'disabled': state.peerInfo.addresses ? undefined : true,
                 'type': 'text',
-                'placeholder': 'Multiaddr',
+                'placeholder': 'CID/path/to/data',
+                'value': 'z43AaGEymG8TWXUuZgFVPB1XkvUadjbwv9RtZignh6kWPmkKNFY/number',
               }
             }),
+            h('button', {
+              'attributes': {
+                'disabled': state.peerInfo.addresses ? undefined : true,
+                'type': 'button'
+              },
+              onclick: (event) => {
+                const input = document.querySelector('#resolve-container input')
+                actions.resolveIpldPath(input.value)
+              },
+            }, `Resolve Path`),
+          ]),
+
+          // block inventory
+          h('#block-container.panel', [
+            Object.keys(state.blocks).map((blockNumber) => {
+              const block = state.blocks[blockNumber]
+              return h('div', `block: #${parseInt(blockNumber)} cid: ${block.cid}`)
+            })
+          ]),
+
+          // block bridging
+          h('#bridge-control.panel', [
+            h('button#start', {
+              'attributes': {
+                'type': 'button',
+                'disabled': !state.isRpcSyncing ? undefined : true,
+              },
+              onclick: actions.startTracker,
+            }, `Start Eth-RPC Bridge`),
             `
               `,
-            h('button.connect-peer', {
-              disabled: state.peerInfo.addresses ? undefined : true,
-              onclick: actions.connectToPeer,
-            }, `Connect to peer`)
-          ])
-        ]),
-        h('div.clear'),
+            h('button#stop', {
+              'attributes': {
+                'disabled': state.isRpcSyncing ? undefined : true,
+                'type': 'button'
+              },
+              onclick: actions.stopTracker,
+            }, `Stop Eth-RPC Bridge`),
+          ]),
 
-        h('#resolve-container.panel', [
-          h('h2', `Resolve from network`),
-          h('input', {
-            'attributes': {
-              'disabled': state.peerInfo.addresses ? undefined : true,
-              'type': 'text',
-              'placeholder': 'CID/path/to/data',
-              'value': 'z43AaGEymG8TWXUuZgFVPB1XkvUadjbwv9RtZignh6kWPmkKNFY/number',
-            }
-          }),
-          h('button', {
-            'attributes': {
-              'disabled': state.peerInfo.addresses ? undefined : true,
-              'type': 'button'
-            },
-            onclick: (event) => {
-              const input = document.querySelector('#resolve-container input')
-              actions.resolveIpldPath(input.value)
-            },
-          }, `Resolve Path`),
-        ]),
+          // peer status
+          h('div.left.panel', [
+            h('div#details'+ state.peerInfo.addresses ? '' : '.disabled', [
+              h('h2', `Your daemon`),
+              h('h3', `ID`),
+              h('pre.id-container', state.peerInfo.id),
+              h('h3', `Addresses`),
+              h('ul.addresses-container', [(
+                state.peerInfo.addresses ?
+                  state.peerInfo.addresses.map((address) => h('li', [h('span.address', address)]))
+                : h('li', `Not yet online`)
+              )])
+            ])
+          ]),
+          h('div.right.panel', [
+            h('div#peers'+ state.peers.length ? '' : '.disabled', [
+              h('h2', 'Remote Peers'),
+              (
+                state.peers.length ?
+                  h('ul', [
+                    state.peers.map((peer) => h('li', peer.addr.toString()))
+                  ])
+                : h('i', 'Waiting for peers...')
+              ),
+            ]),
+            h('div', [
+              h('input.connect-peer', {
+                'attributes': {
+                  'disabled': state.peerInfo.addresses ? undefined : true,
+                  'type': 'text',
+                  'placeholder': 'Multiaddr',
+                }
+              }),
+              `
+                `,
+              h('button.connect-peer', {
+                disabled: state.peerInfo.addresses ? undefined : true,
+                onclick: actions.connectToPeer,
+              }, `Connect to peer`)
+            ])
+          ]),
+          h('div.clear'),
 
-        h('#block-container.panel', [
-          h('button#start', {
-            'attributes': {
-              'type': 'button',
-              'disabled': !state.isRpcSyncing ? undefined : true,
-            },
-            onclick: actions.startTracker,
-          }, `Start RPC Sync`),
-          `
-            `,
-          h('button#stop', {
-            'attributes': {
-              'disabled': state.isRpcSyncing ? undefined : true,
-              'type': 'button'
-            },
-            onclick: actions.stopTracker,
-          }, `Stop RPC Sync`),
-          Object.keys(state.blocks).map((blockNumber) => {
-            const block = state.blocks[blockNumber]
-            return h('div', `block: ${blockNumber} cid: ${block.cid}`)
-          })
-        ]),
-
-        h('pre#errors',
-          state.error
-        ),
+          // errors
+          h('pre#errors',
+            state.error
+          ),
+        ])
       ])
-    ])
 
+    ])
   )
 }
